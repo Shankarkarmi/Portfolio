3ffb3d53b740095e323e78f3834c2d176a4a49ac
Shankar Karmi <shankarkarmi52@gmail.com>
2024-11-07 17:12:44 +0530

    Your commit second

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants/index";
-import profilePic from "../assets/images/ShankarKarmiProfile.jpg";
+import profilePic from "../assets/images/ShankarKarmi.png";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ import { FaSquareTwitter } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
-    <nav className="mb-20 w-full flex justify-between items-center py-6 px-10 lg:px-20">
+    <nav className="mb-20 w-full flex justify-between items-center py-6 px-10 lg:px-20 ">
       <div className="flex items-center">
-        <img className="mx-2" width={60} src={logo} alt="Shankar Karmi Logo" />
+        <img className="mx-2" width={60} src={logo} alt="Shankar Karmi Logo"/>
       </div>
       <div className="flex m-8 items-center justify-center gap-4 text-2xl">
         <a href="https://www.linkedin.com/in/shankar-karmi-878914262/" target="_blank" rel="noopener noreferrer">
